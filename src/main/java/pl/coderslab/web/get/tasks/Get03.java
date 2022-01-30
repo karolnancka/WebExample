f063c66333c6eda4f9bcdbe9f883b544f26acaab
karol <karolnancka@gmail.com>
2022-01-30 17:03:06 +0100

    GET Task03

---
+++ src/main/java/pl/coderslab/web/get/tasks/Get03.java
@@ -0,0 +1,48 @@
+package pl.coderslab.web.get.tasks;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@WebServlet("/get3")
+public class Get03 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String widthStr = req.getParameter("width");
+        String heightStr = req.getParameter("height");
+
+        try {
+
+
+            int width = Integer.valueOf(widthStr);
+            int height = Integer.valueOf(heightStr);
+            for (int i = 1; i <= width; i++) {
+                for (int j = 1; j <= height; j++) {
+                    resp.getWriter().print(j * i + " ");
+                }
+                resp.getWriter().println();
+            }
+
+
+        } catch (NumberFormatException e) {
+            //resp.getWriter().println("Incorrect request data. Input numbers in parameters: start and end");
+            //resp.getWriter().println(String.format("start=%s, end=%s", heightStr, widthStr));
+
+            int width = 5;
+            int height = 10;
+            for (int i = 1; i <= width; i++) {
+                for (int j = 1; j <= height; j++) {
+                    resp.getWriter().print(j * i + " ");
+                }
+                resp.getWriter().println();
+            }
+
+
+        }
+    }
+}
