12a700ba3a9cea523fa6739e0220829bfdc3ac73
karol <karolnancka@gmail.com>
2022-01-30 16:06:32 +0100

    Servlety Task04

--- src/main/java/pl/coderslab/web/Servlet14.java
+++ src/main/java/pl/coderslab/web/Servlet14.java
@@ -1,4 +1,17 @@
 package pl.coderslab.web;
 
-public class Servlet14 {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/servlet14", name = "Servlet14 Header")
+public class Servlet14 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //resp.getWriter().println(req.getHeader(getServletName()));
+        //resp.getWriter().println();
+    }
 }
